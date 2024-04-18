8d20499d3284a9a437012df13aff0c13455b24d9
Алексей Степаненко <130142468+AlekseiStepanenko@users.noreply.github.com>
2024-04-18 21:40:14 +0300

    Update tests.py

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,14 +1,13 @@
 from unittest import TestCase
-# from rest_framework.test import APIClient
+from rest_framework.test import APIClient
 
 
 class MyTest(TestCase):
     def test_ok(self):
-        assert True
-#
-#
-    # def test_sample_view(self):
-    #     url = '/api/v1/test/'
-    #     client = APIClient()
-    #     response = client.get(url)
-    #     assert response.status_code == 200
+        self.assertTrue(True)
+
+    def test_sample_view(self):
+        url = '/api/v1/'
+        client = APIClient()
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
